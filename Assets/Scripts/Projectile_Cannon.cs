399b00c744c097d5ae49fb65d0ee07bde6273e21
jchansen <jason_hansen@outlook.com>
2014-02-22 15:01:20 -0700

    initial commit

---
+++ Assets/Scripts/Projectile_Cannon.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile_Cannon : MonoBehaviour {
+
+	public float mySpeed = 10.0f;
+	public float myRange = 10.0f;
+	private float myDist = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * Time.deltaTime * mySpeed);
+		myDist += Time.deltaTime * mySpeed;
+		if (myDist >= myRange) {
+			Destroy(gameObject);
+		}
+	}
+}
